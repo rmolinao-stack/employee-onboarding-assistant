114978fe1de86b03be41f7dec22854a0aab27c74
rmolinao <rmolinao@gmail.com>
2026-07-11 16:27:05 +0200

    feat: Move contants & utils to common

--- model/objects/empresa.py
+++ model/objects/empresa.py
@@ -1,8 +1,9 @@
-import utils
+import common.utils as utils
+import common.constants as constants
 
 class Empresa:
-    def __init__(self, json_path):
-        self.data = utils.cargar_json(json_path)
+    def __init__(self):
+        self.data = utils.cargar_json(constants.EMPLEADOS_DEMO_PATH)
             
     def getEmpresa(self) -> dict:
         return self.data
